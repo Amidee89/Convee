a293f45f38898dc360042635aaf29a295b137f47
Marco Carandente <marco.carandente@gmail.com>
2024-08-17 23:46:31 +0300

    Initial Commit

---
+++ Convee/ConveeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConveeApp.swift
+//  Convee
+//
+//  Created by Marco Carandente on 17.8.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConveeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
